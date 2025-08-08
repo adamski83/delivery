5979d3ccdb70fb9fd0859c77ea20a12a62815ba3
adamski83 <adamsik83@wp.pl>
2025-08-08 19:52:56 +0200

    refactor: enhance HomeBanner layout and styling for better presentation

--- components/homeBanner/HomeBanner.tsx
+++ components/homeBanner/HomeBanner.tsx
@@ -1,15 +1,23 @@
 import Link from "next/link";
-import React from "react";
 
+import banner_1 from "@/images/banner/banner_1.png";
+import Image from "next/image";
 import { Title } from "../text/text";
 import styles from "./HomeBanner.module.css";
 
 const HomeBanner = () => {
   return (
     <div className={styles.wrapper}>
-      <div>
-        <Title>Grab Upto 50% Off On Selected Headphone</Title>
-        <Link href={"/shop"}>Buy Now</Link>
+      <div className={styles.title}>
+        <Title>
+          Grab Upto 50% Off On <br /> Selected Headphone
+        </Title>
+        <Link href={"/shop"} className={styles.button}>
+          Buy Now
+        </Link>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src={banner_1} alt="Home Banner" className={styles.image} />
       </div>
     </div>
   );
