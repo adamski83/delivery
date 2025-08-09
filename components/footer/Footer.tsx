8001b1e43abf818f12128d3603a0b89beda91e02
adamski83 <adamsik83@wp.pl>
2025-08-09 15:18:42 +0200

    refactor: update ESLint rules and add cleaning scripts for improved code quality

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,9 +1,12 @@
 import Container from "../Container/Container";
+import FooterTop from "./FooterTop";
 
 const Footer = () => {
   return (
-    <footer>
-      <Container> Footer</Container>
+    <footer className="border-t bg-green-500">
+      <Container>
+        <FooterTop />
+      </Container>
     </footer>
   );
 };
---
+++ components/footer/FooterTop.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FooterTop = () => {
+  return <div>Footer top</div>;
+};
+
+export default FooterTop;
