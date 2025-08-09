22577d12af677d67702e239b5742b171933303b6
adamski83 <adamsik83@wp.pl>
2025-08-09 16:00:56 +0200

    refactor: standardize CSS variable naming and improve component styles

--- components/homeBanner/HomeBanner.tsx
+++ components/homeBanner/HomeBanner.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
-
 import banner_1 from "@/images/banner/banner_1.png";
 import Image from "next/image";
+import Link from "next/link";
+
 import { Title } from "../text/text";
 import styles from "./HomeBanner.module.css";
 
@@ -9,15 +9,19 @@ const HomeBanner = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>
-        <Title>
-          Grab Upto 50% Off On <br /> Selected Headphone
+        <Title className={styles.title}>
+          Grab Upto 50% Off On Selected Headphone
         </Title>
-        <Link href={"/shop"} className={styles.button}>
+        <Link href="/shop" className={styles.button}>
           Buy Now
         </Link>
       </div>
       <div className={styles.imageContainer}>
-        <Image src={banner_1} alt="Home Banner" className={styles.image} />
+        <Image
+          src={banner_1}
+          alt="A girl with Headphones"
+          className={styles.image}
+        />
       </div>
     </div>
   );
