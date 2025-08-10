6f46d13c1f3116bb77d8b303c51e0b43daaee9ac
adamski83 <66271771+adamski83@users.noreply.github.com>
2025-08-10 10:54:07 +0200

    closed footer  componen, remove tailwind class.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,12 +2,13 @@ import Container from "../Container/Container";
 import FooterTop from "./FooterTop";
 
 const Footer = () => {
-  return (
-    <footer className="border-t bg-green-500">
-      <Container>
-        <FooterTop />
-      </Container>
-      )
-      };
+	return (
+		<footer>
+			<Container>
+				<FooterTop />
+			</Container>
+		</footer>
+	);
+};
 
 export default Footer;
