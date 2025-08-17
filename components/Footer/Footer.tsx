fed5bcb9f3e90dc2df36160c53fb68e2fc3e5e19
adamski83 <adamsik83@wp.pl>
2025-08-17 19:50:42 +0200

    Refactor footer and related components; update styles and add category page

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,87 @@
+import { categoriesData, quickLinksData } from "@/constants";
+import Link from "next/link";
+
 import Container from "../Container/Container";
+import Logo from "../Logo/Logo";
+import SocialMedia from "../SocialMedia/SocialMedia";
+import { Title } from "../Text/Text";
+import styles from "./Footer.module.css";
 import FooterTop from "./FooterTop";
+import { Button, Input } from "@mui/material";
 
 const Footer = () => {
   return (
     <footer>
-      <Container>
+      <Container className={styles["container-footer"]}>
         <FooterTop />
+        <div className={styles.footerGrid}>
+          <div className={styles.spaceY4}>
+            <Logo />
+            <Title as="p" className={styles.text}>
+              Discover the Best what we have to offer.
+            </Title>
+            <SocialMedia />
+          </div>
+          <div>
+            <Title as="h3" className={styles.title}>
+              Quick Links
+            </Title>
+            <ul className={styles.listContainer}>
+              {quickLinksData?.map(item => (
+                <li key={item?.title}>
+                  <Link href={item?.href} className={styles.link}>
+                    {item?.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <Title as="h3" className={styles.title}>
+              Categories
+            </Title>
+            <ul className={styles.listContainer}>
+              {categoriesData?.map(item => (
+                <li key={item?.title}>
+                  <Link
+                    href={`/category/${item?.href}`}
+                    className={styles.link}
+                  >
+                    {item?.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.spaceY4}>
+            <Title as="h3" className={styles.title}>
+              Contact Us
+            </Title>
+            <p className={styles.link}>Subscribe to our newsletter</p>
+            <form className={styles.form}>
+              <Input
+                type="email"
+                placeholder="Enter your email"
+                required
+                className={styles.input}
+              />
+              <Button
+                sx={{
+                  backgroundColor: "#000000",
+                  color: "#ffffff",
+                  "&:hover": {
+                    backgroundColor: "#374151",
+                  },
+                }}
+              >
+                Subscribe
+              </Button>
+            </form>
+          </div>
+        </div>
+        <div className={styles["footer-bottom"]}>
+          {new Date().getFullYear()} © Delivery <Logo />
+        </div>
       </Container>
     </footer>
   );
--- components/Footer/FooterTop.tsx
+++ components/Footer/FooterTop.tsx
@@ -1,7 +1,50 @@
+import { Clock, Mail, MapPin, Phone } from "lucide-react";
 import React from "react";
 
+import styles from "./Footer.module.css";
+
+interface FooterTopProps {
+  title: string;
+  subtitle: string;
+  icon: React.ReactNode;
+}
+
+const data: FooterTopProps[] = [
+  {
+    title: "Visit Us",
+    subtitle: "Berlin, Germany",
+    icon: <MapPin />,
+  },
+  {
+    title: "Call Us",
+    subtitle: "+1-234-567-890",
+    icon: <Phone />,
+  },
+  {
+    title: "Working Hours",
+    subtitle: "Mon - Fri: 7:00 - 17:00",
+    icon: <Clock />,
+  },
+  {
+    title: "Email Us",
+    subtitle: "fakedata@.gmail.com",
+    icon: <Mail />,
+  },
+];
 const FooterTop = () => {
-  return <div>Footer top</div>;
+  return (
+    <div className={styles.container}>
+      {data?.map((item, index) => (
+        <div key={index} className={styles.item}>
+          {item.icon}
+          <div>
+            <h3 className={styles["item-h3"]}>{item?.title}</h3>
+            <p className={styles.paragrapf}>{item?.subtitle}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default FooterTop;
